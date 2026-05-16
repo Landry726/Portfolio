399142373f0203f51f0d6ef03cb7d3ce43db23f1
landry <hajaniainalandry.com>
2026-05-16 19:28:30 +0300

    Changement du CV

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -32,7 +32,7 @@ function Home() {
           </a> */}
           
           <a 
-          href="/public/CV Landry.pdf"
+          href="/CV_Landry.pdf"
           download="CV Landry.pdf"
           className="btn  btn-accent btn-lg shadow-lg w-full md:w-fit hover:scale-105 transition-transform"
           >
